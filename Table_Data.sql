2afe22a3e12ddee9191b1b950ec9e6a3c74c3fac
David Steffen <112430270+DavidSteffen1@users.noreply.github.com>
2023-01-15 10:24:38 -0800

    Add files via upload
    Table insert statements for basic armor, fighting styles, and weapons

---
+++ Table_Data.sql
@@ -0,0 +1,14 @@
+INSERT INTO armor (armor_id, armor_name, armor_effectiveness, armor_location)
+VALUES (1, 'Wizard Hat', NULL, 'head'),
+(2, 'Chain Helmet', NULL, 'head'),
+(3, 'Archers Cap', NULL, 'head');
+
+INSERT INTO fighting_style (style_id, style_name, style_effectiveness)
+VALUES (1, 'Magic', NULL),
+(2, 'Warrior', NULL),
+(3, 'Archery', NULL);
+
+INSERT INTO weapons (weapon_id, weapon_name, weapon_effectiveness)
+VALUES (1, 'Wand', NULL),
+(2, 'Axe', NULL),
+(3, 'Bow', NULL);
