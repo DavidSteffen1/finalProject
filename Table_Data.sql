6f24a8a87ef7f705b120f1e43e3fc7c2df408e99
David Steffen <112430270+DavidSteffen1@users.noreply.github.com>
2023-01-15 16:40:37 -0800

    Update Table_Data.sql

--- Table_Data.sql
+++ Table_Data.sql
@@ -3,11 +3,6 @@ VALUES (1, 'Wizard Hat', NULL, 'head'),
 (2, 'Chain Helmet', NULL, 'head'),
 (3, 'Archers Cap', NULL, 'head');
 
-INSERT INTO fighting_style (style_id, style_name, style_effectiveness)
-VALUES (1, 'Magic', NULL),
-(2, 'Warrior', NULL),
-(3, 'Archery', NULL);
-
 INSERT INTO weapons (weapon_id, weapon_name, weapon_effectiveness)
 VALUES (1, 'Wand', NULL),
 (2, 'Axe', NULL),
