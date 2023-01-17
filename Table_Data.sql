a5941d5fd4bd091191c1fbc48cf35497e783dd0f
David Steffen <112430270+DavidSteffen1@users.noreply.github.com>
2023-01-16 19:03:11 -0800

    Update Table_Data.sql

--- Table_Data.sql
+++ Table_Data.sql
@@ -7,3 +7,8 @@ INSERT INTO weapons (weapon_id, weapon_name, weapon_effectiveness)
 VALUES (1, 'Wand', NULL),
 (2, 'Axe', NULL),
 (3, 'Bow', NULL);
+
+INSERT INTO character (character_id, name, fighting_style)
+VALUES (1, 'Gandalf', MAGIC), 
+(2, 'Sun Tzu', WARRIOR), 
+(3, 'Robin Hood', ARCHERY);
